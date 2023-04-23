6a98b99d23d002c9f5695041e237d6715ed9e05b
Reece Pena <Jalapena42@gmail.com>
2023-04-22 21:47:12 -0400

    Delete SGK, Character BP Setup

---
+++ Source/AloneAgain/Public/MyGenPawn.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GenPawn.h"
+#include "MyGenPawn.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ALONEAGAIN_API AMyGenPawn : public AGenPawn
+{
+	GENERATED_BODY()
+	
+};
